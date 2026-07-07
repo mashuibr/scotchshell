45787ab74f0e56dee647086833758621a340b57f
mashu <mashuibr@gmail.com>
2026-07-07 15:45:13 +0000

    Implement basic shell functionality with echo command and exit option; add command and commands header files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
+//Author - Ibrahim Shah
+//Date Written - 07/07/26
+//hey yooo
+
 #include <iostream>
+#include<string>
+
+using namespace std;
+
 int main(){
-    using namespace std;
-    cout << "Hello World!!\n";
-    return 0;
+
+    string command;
+
+    while(true){
+
+        cout << "my shell> ";//test print
+
+        getline(cin, command); //get user input from shell
+
+        if(command == "exit"){
+            break; //exit shell with exit as command
+        }
+
+        cout << "You typed: "<<command << endl; //put user input to shell
+
+        if(command.substr(0,4)=="echo"){
+            cout << command.substr(5) <<endl; //echo command to shell
+        }
+
+    }
+
 }
